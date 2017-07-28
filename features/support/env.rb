5d67220867269a9df4809971a06996cd6fb3c886
Wanderley Barroso <wanderley.barroso@concrete.com.br>
2017-07-28 18:57:02 -0300

    Config para as duas plataform

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,3 @@
-# This file provides setup and common functionality across all features.  It's
-# included first before every test run, and the methods provided here can be 
-# used in any of the step definitions used in a test.  This is a great place to
-# put shared data like the location of your app, the capabilities you want to
-# test with, and the setup of selenium.
-
 require 'rspec/expectations'
 require 'appium_lib'
 require 'cucumber/ast'
@@ -12,37 +6,18 @@ require 'cucumber/ast'
 class AppiumWorld
 end
 
-# Load the desired configuration from appium.txt, create a driver then
-# Add the methods to the world
-caps = Appium.load_appium_txt file: File.expand_path('./../../../appium.txt', __FILE__), verbose: true
+if ENV['PLATFORM'] == 'ios'
+  caps = Appium.load_appium_txt file: File.expand_path('./../../../iosappium.txt', __FILE__), verbose: true
+elsif ENV['PLATFORM'] == 'android'
+  caps = Appium.load_appium_txt file: File.expand_path('./../../../AndroidAppium.txt', __FILE__), verbose: true
+end
+
 Appium::Driver.new(caps)
 Appium.promote_appium_methods AppiumWorld
-
-
-# if ENV['IDEVICENAME']=='ipad simulator'
-# 	caps = Appium.load_appium_txt file: File.expand_path("./../ipadsim/appium.txt", __FILE__), verbose: true
-# elsif ENV['IDEVICENAME']=='iphone simulator'
-# 	caps = Appium.load_appium_txt file: File.expand_path("./../iphonesim/appium.txt", __FILE__), verbose: true
-# else
-# caps = Appium.load_appium_txt file: File.expand_path("../appium.txt", __FILE__), verbose: true
-# end
 
 World do
   AppiumWorld.new
 end
 
-
 Before { $driver.start_driver }
 After { $driver.driver_quit }
-
-
-=begin
-# If you wanted one env.rb for both android and iOS, you could use logic similar to this:
-world_class = ENV['PLATFORM_NAME'] == 'iOS' ? IosWorld : AndroidWorld
-# each world class defines the `caps` method specific to that platform
-Appium::Driver.new world_class.caps
-Appium.promote_appium_methods world_class
-World { world_class.new }
-Before { $driver.start_driver }
-After { $driver.driver_quit }
-=end
